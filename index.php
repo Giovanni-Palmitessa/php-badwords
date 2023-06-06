a75376dcfec1d5fdf7d4e417da98a00601616a33
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-06-06 16:04:47 +0200

    add variable in index.php

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
+<?php 
+    $word_to_censure = $_GET['word'];
+?>
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -7,7 +10,7 @@
     <title>Bad Words</title>
   </head>
   <body>
-    <h1>Parola da censurare: <?= $_GET['word']; ?></h1>
+    <h1>Parola da censurare: <?= $word_to_censure; ?></h1>
     <p>Paragrafo originale: <?= $_GET['paragraph']?></p>
   </body>
 </html>
